c018bb882ba05862b1655e3801e3dad80ff89d8e
Vulcan <vulcan.lee@gmail.com>
2017-06-14 10:51:04 +0800

    Xamarin.Android 開發練習

---
+++ XAndroid1/XAndroid1/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XAndroid1
+{
+    [Activity(Label = "XAndroid1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            EditText fooEditText = FindViewById<EditText>(Resource.Id.editText1);
+            Button fooButton = FindViewById<Button>(Resource.Id.button1);
+            TextView fooTextView = FindViewById<TextView>(Resource.Id.textView1);
+
+            fooButton.Click += (s, e) =>
+                {
+                    fooTextView.Text = fooEditText.Text;
+                };
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
