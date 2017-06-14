fa7f971421c2afe45637b74fa8b18609e73aa6d1
Vulcan <vulcan.lee@gmail.com>
2017-06-14 12:30:03 +0800

    XF Code Behind

---
+++ XForms/XForms/XForms/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            myButton.Clicked += (s, e) =>
+            {
+                myLabel.Text = myEntry.Text;
+            };
+        }
+
+        //private void myButton_Clicked(object sender, EventArgs e)
+        //{
+        //    myLabel.Text = myEntry.Text;
+        //}
+    }
+}
