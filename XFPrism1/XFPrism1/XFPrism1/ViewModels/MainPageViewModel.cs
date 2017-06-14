7cf32f4a2b91b15da7e6585f3d7e8f556264407d
Vulcan <vulcan.lee@gmail.com>
2017-06-14 13:38:47 +0800

    Xamarin.Form MVVM

---
+++ XFPrism1/XFPrism1/XFPrism1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace XFPrism1.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Title { get; set; }
+        public string fooEntry { get; set; }
+        public string fooLabel { get; set; }
+
+        public DelegateCommand OKCommand { get; set; }
+        public MainPageViewModel()
+        {
+            OKCommand = new DelegateCommand(() =>
+            {
+                fooLabel = fooEntry;
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ XFPrism1/XFPrism1/XFPrism1/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFPrism1.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
